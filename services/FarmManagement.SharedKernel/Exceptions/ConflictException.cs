4e2d329ff63a39ffd923d84940d1930629366282
Aditya Gitte <BE21F05F028@geca.ac.in>
2026-08-14 00:40:21 +0530

    FMS-212: Refactor Catalog API Livestock Domain and add test suite

---
+++ services/FarmManagement.SharedKernel/Exceptions/ConflictException.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace FarmManagement.SharedKernel.Exceptions;
+
+public class ConflictException : Exception
+{
+    public ConflictException(string message) : base(message)
+    {
+    }
+
+    public ConflictException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
